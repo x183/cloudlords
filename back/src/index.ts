1387a3b7a91f7f802e0558425b30e34d4ef81721
Oscar Palm <palmoscar4@gmail.com>
2024-12-20 14:18:08 +0100

    Finish fetch script

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import env from 'dotenv';
 import cloudService from './services/cloudService';
-import cors from 'cors';
+
 
 env.config();
 const app = express()
 const port = process.env.PORT || 3000;
-app.use(cors());
+
 
 app.get('/', (req, res) => {
 	res.send('Hello World!')
--- scripts/collector.py
+++ scripts/collector.py
@@ -1,23 +1,64 @@
 #! /bin/python3
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
+import csv
+import json
 
 
-def fetch_file_data(file_name):
-	pass
+files_to_use = ["e form (Responses)"]
 
-def login_to_drive():
-	gauth = GoogleAuth()
-	gauth.LocalWebserverAuth()
+
+def main():
+	drive = authenticate()
+	file_list = drive.ListFile().GetList()
+	for file in file_list:
+		if file["title"] in files_to_use:
+			print('title: %s, id: %s' % (file['title'], file['id']))
+			fetch_file_data(file)
+	make_json(files_to_use)
+
+
+
+
+def fetch_file_data(file):
+	file.GetContentFile(f'{file["title"]}.csv',mimetype='text/csv')
+
+def authenticate():
+	settings = {
+		"client_config_backend": "service",
+			"service_config": {
+				"client_json_file_path": "credentials.json",
+			}
+		}
+	# Create instance of GoogleAuth
+	gauth = GoogleAuth(settings=settings)
+	# Authenticate a service user
+	gauth.ServiceAuth()
+
 	drive = GoogleDrive(gauth)
 	return drive
 
-def main():
-	drive = login_to_drive()
-	file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
-	for file1 in file_list:
-  		print('title: %s, id: %s' % (file1['title'], file1['id']))
 
+
+def read_csv(file_name):
+	data = []
+	with open(f"{file_name}.csv", encoding='utf-8') as csv_file:
+		csvReader = csv.DictReader(csv_file)
+		for rows in csvReader:
+			rows["type"] = file_name
+			data.append(rows)
+	return data
+
+def make_json(file_names):
+	data = []
+	for file_name in file_names:
+		file_data = read_csv(file_name)
+		data.append(file_data)
+	write_json("form_data",data)
+
+def write_json(file_name,data):
+	with open(f"{file_name}.json", 'w', encoding='utf-8') as jsonf:
+		jsonf.write(json.dumps(data, indent=4))
 
 
 if __name__ == "__main__":
